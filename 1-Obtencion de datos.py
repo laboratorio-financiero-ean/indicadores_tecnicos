dfd9da0be9875dd7d5d14ab97668e90c17f529b9
LaboratorioFinancieroEAN <jmorale61310@universidadean.edu.co>
2022-09-20 19:55:12 -0500

    Actualizacion del proyecto

---
+++ 1-Obtencion de datos.py
@@ -0,0 +1,12 @@
+
+# Repositorio de algoritmos basado en indicadores técnicos  
+#
+# Universidad EAN, (2022)
+# Laboratorio financiero
+# Autor: Jose Rafael Morales Parra
+# twitter.com/aDataScienceMan       
+# linkedin.com/in/adatascienceman/ 
+#
+# Disclaimer legal.
+# Abordamos los temas de análisis de mercados financieros desde el ámbito académico 
+# En ningún caso podrá tomarse este contenido académico como consejos de inversión o asesoría financiera.
