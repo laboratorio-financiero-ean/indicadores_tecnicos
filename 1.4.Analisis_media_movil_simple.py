1642de56dfb89ac1f7727312dd608da25233ee73
LaboratorioFinancieroEAN <jmorale61310@universidadean.edu.co>
2022-10-05 22:34:56 -0500

    Generacion de Medias moviles basado en el Análisis de la accion de Google

--- 1.4.Analisis_media_movil_simple.py
+++ 1.4.Analisis_media_movil_simple.py
@@ -3,41 +3,51 @@
 # Universidad EAN, (2022)
 # Laboratorio financiero
 # Autor: Jose Rafael Morales Parra
-# twitter.com/aDataScienceMan       
-# linkedin.com/in/adatascienceman/ 
+# twitter.com/aDataScienceMan           # linkedin.com/in/adatascienceman/ 
 #
 # Disclaimer legal.
 # Abordamos los temas de análisis de mercados financieros desde el ámbito académico 
 # En ningún caso podrá tomarse este contenido académico como consejos de inversión o asesoría financiera.
 #
-# Resultado: Analisis de la accion de Google, Identificación de soportes y resistencias
+# Resultado: Generacion de Medias moviles basado en el Análisis de la accion de Google
  
 
+import datetime as date
 import numpy  as np
 import pandas as pd
 from pandas_datareader import data as pdr
-import datetime as date
 import matplotlib.pyplot as plt
 
-simbolos=['SAN.MC', 'BBVA.MC']
-desde   = '2022-01-01'   # Enero 1/2022
+#simbolos=['SAN.MC', 'BBVA.MC']
+simbolos='GOOGL'
+desde   = '2018-01-01'   # Enero 1/2022
 hasta   = '2022-09-30'   # Septiembre  30/2022
 
-datos=pdr.get_data_yahoo(simbolos, start=desde, end=hasta)
-
+datos=pdr.get_data_yahoo(simbolos, start = desde, end = hasta)
+# Lo que recibimos en data es un Panel de Pandas, una matriz de dos dimensiones
+datos.head()
 print(datos)
 
-datos.head()
-%matplotlib inline
-datos.Close.plot()
+# Dimensiones del dataFrame recibido 
+print(datos.shape)
 
-# Calculo de la media movil simple
+#%matplotlib inline# Impresion grafico 1
+#datos.Close.plot()
 
-
-def MA(df,n):
-    MA = pd.Series(pd.Series.rolling(df['Close'], n).mean(), name = 'MA_' + str(n))   
-    df = df.join(MA)
+    
+def MediaMovil(df,periodos):
+    MediaMovil = pd.Series(pd.Series.rolling(df['Close'], periodos).mean(), name='MA_' + str(periodos)) 
+    df = df.join(MediaMovil)
     return df
 
-mm_10  = MA(datos,10) 
-mm10.head() 
+#ma_9   = MediaMovil(datos,9)      # Acción del precio
+ma_50  = MediaMovil(datos, 50)      # Reversiones
+ma_200 = MediaMovil(ma_50, 200)     # Tendencia principal
+
+ma_200.head()
+
+df = ma_200[['Close','MA_50','MA_200']]
+
+titulo = 'Acción de '+ simbolos +':: Gráfico de Medias moviles desde:'+ desde + ' hasta:'+ hasta 
+df.plot(title = titulo , figsize = (16,8))    
+
